bfdc668b39c5ab784ec0c50539bbf7019d649c6e
elon-hangyang <dahang@Dahangs-MBP.attlocal.net>
2020-09-23 00:23:06 -0500

    delete config & .env file from remote repo

--- backend/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: 3002,
-  ATLAS_URI:
-    "mongodb+srv://adahang:Ada@1988ada@cluster0.nxk6i.mongodb.net/kidcare?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-};
